390f527578b5efb4275b3a50651ba42b1f0111d1
Chamath Samarawickrama <chamaths@wso2.com>
2020-07-31 17:33:37 +0530

    add new component for tomcat filter

--- components/org.wso2.carbon.identity.sso.tomcat.server/src/main/java/org/wso2/carbon/identity/sso/tomcat/server/SSOAgentContextEventListener.java
+++ components/org.wso2.carbon.identity.sso.tomcat.server/src/main/java/org/wso2/carbon/identity/sso/tomcat/server/SSOAgentContextEventListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-package org.wso2.carbon.identity.sso.agent;
+package org.wso2.carbon.identity.sso.tomcat.server;
 
 import org.apache.commons.lang.StringUtils;
 import org.wso2.carbon.identity.sso.agent.bean.SSOAgentConfig;
